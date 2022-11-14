5e44d2eea9dcb17972cf7c9eaa0e43d22679b1f7
Dipendra1896 <dipendrapatel371896@gmail.com>
2022-11-15 03:02:58 +0545

    New code

---
+++ 4th git repo/c++.cpp
@@ -0,0 +1,19 @@
+<include><iostream>
+using name space std;
+class Number
+{
+    public:
+    int a,b,sum;
+    void add()
+    {
+        cout<<"enter two numbers"<<endl;
+        cin>>a>>b;
+        sum=a+b;
+        cout<<"sum is:"<<sum<<endl;
+    }
+};
+main()
+{
+    Number ob;
+    ob.add();
+}
